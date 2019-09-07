f1bfb782bd94ebf0687ba26f3c96ec8c5db3974a
m <dev.cheuk.lee@gmail.com>
2019-09-07 16:46:55 -0400

    implement apis for automatically create pages for MD files

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,49 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const {createFilePath} = require(`gatsby-source-filesystem`)
+const {}
+
+// create a path or slug for each page
+exports.onCreateNode = ({ node, getNode, boundActionCreators}) => {
+    const { createNoteField } = boundActionCreators
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode, basePath: `pages` })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug
+        })
+    }
+}
+
+// create page for each slug
+exports.createPages = ({graphql, boundActionCreators}) => {
+    const {createPage} = boundActionCreators
+    return new Promise((resolve, reject) => {
+        graphql(`
+            {
+                allMarkdownRemark {
+                    edges {
+                        node {
+                            fields {
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `).then(result => {
+            result.data.allMarkdownRemark.edges.forEach(({node}) => {
+                createPage({
+                    path: node.fields.slug,
+                    component: path.resolve(`./src/templates/post.js`),
+                    context: {
+                        slug: node.fields.slug
+                    }
+                })
+            })
+            resolve()
+        })
+    })
+}
