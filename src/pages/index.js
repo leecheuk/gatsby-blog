d0e104a805bf8752f38868d00d938a4113149798
m <dev.cheuk.lee@gmail.com>
2019-09-07 16:44:47 -0400

    changed index to show list of blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,43 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({data}) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {data.allMarkdownRemark.edges.map(({node}) => (
+        node ? <div className="blog-post-container" key={node.id}>
+          <div className="blog-post">
+            <Link to={node.frontmatter.path}><h3>{node.frontmatter.title}</h3></Link>
+            <p className="author">{node.frontmatter.author}</p>
+            <p className="excerpt">{node.excerpt}</p>
+          </div>
+        </div> : null
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const  query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+          node {
+            frontmatter {
+              path
+              title
+              date
+              author
+            }
+            excerpt
+            timeToRead
+          }
+      }
+    }
+  }
+`
